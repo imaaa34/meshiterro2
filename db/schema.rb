4b76f0c4b98726ce5a4edfdd15b6ffc296480ce8
imaaa34 <oct.imd34@gmail.com>
2021-05-29 14:07:42 +0000

    [Update]投稿詳細ページのいいね非同期機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_05_23_132419) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.boolean "is_deleted"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
