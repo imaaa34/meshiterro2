03ebb82756c118aaa52f11e075cd1b03a972879c
imaaa34 <oct.imd34@gmail.com>
2021-06-10 05:51:27 +0000

    [Add] namespaceで管理者画面を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_124500) do
+ActiveRecord::Schema.define(version: 2021_06_10_052611) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_124500) do
     t.string "profile_image_id"
     t.boolean "is_deleted"
     t.boolean "is_valid", default: true, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
