f57270eceaa3bd59eeafd000165b508243c3a5ec
imaaa34 <oct.imd34@gmail.com>
2021-06-16 01:05:00 +0000

    [Add] 管理者側フォローフォロワーページ作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -22,6 +22,18 @@ class Admin::UsersController < ApplicationController
     end
   end
   
+  def following
+    @user = User.find(current_user.id)
+    @users = @user.followings
+    render 'show_follow'
+  end
+  
+  def follower
+    @user = User.find(current_user.id)
+    @users = @user.followers
+    render 'show_follower'
+  end
+  
   private
     def if_not_admin
       redirect_to root_path unless current_user.admin?
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: [:index, :show, :edit, :update]
     resources :post_images, only: [:index, :show, :edit]
+    get 'users/show_follow' => 'users#following'
+    get 'users/show_follower' => 'users#follower'    
   end
   get 'unsubscribe/:id' => 'homes#unsubscribe', as: 'confirm_unsubscribe'
   patch 'unsubscribe/:id/withdraw' => 'homes#withdraw', as: 'withdraw_user'
